971bbae41c53b6bc0641efb3f214a984c9051a15
David Carpenter <caprenter@gmail.com>
2013-08-29 14:27:04 +0100

    This makes the refresh script for the registry API into a file that cannot be executed. Prevents people from attacking the server

--- helpers/refresh_group_data.example.php
+++ helpers/refresh_group_data.example.php
@@ -1,5 +1,17 @@
 <?php
-
+/*
+ * 
+ * This file will grab data from the CKAN API to populate the 
+ * data about groups used for the Reporting_org Dropdown
+ * 
+ * Make a copy of this file, and uncomment the code.
+ * Rename it to something that others won't find!
+ * Set your own cache file destination - but beware that 
+ * /include/reporting_org.php needs to find it!
+ * 
+ * This way people can't just hammer your server and the API
+ * by hitting this URL
+ * 
 // Display errors for demo
 @ini_set('error_reporting', E_ALL);
 @ini_set('display_errors', 'stdout');
@@ -41,6 +53,6 @@ function refresh_groups_cache ($ckan,$data,$cachefile) {
   $groups = json_encode($groups);
   file_put_contents($cachefile,$groups);
 }
-  
+*/
 ?>
 	
